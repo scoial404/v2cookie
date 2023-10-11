40543eba64b052b200619861b7695c1eb4afa478
scoial404 <89151779+scoial404@users.noreply.github.com>
2023-10-11 16:56:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('sex'));
+
+app.listen(port, () =>
+  console.log(`Your app is listening a http://localhost:${port}`)
+);
+
 const fs = require('fs');
 const Discord = require('discord.js');
 const mongoose = require('mongoose'); // Import the mongoose module
