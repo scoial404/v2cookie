b4a45b3766565ed51f81e779456117bcc892b682
scoial404 <89151779+scoial404@users.noreply.github.com>
2023-10-11 16:52:26 +0300

    Update h.js

--- commands/h.js
+++ commands/h.js
@@ -7,17 +7,17 @@ module.exports = {
   aliases: ["h"],
   description: "h Command!",
   usage: "h | <Command Name>",
-  run: async(client, message, args) => {
+  run: async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-    .setColor('#3575ed')
-    .addField("__**User:**__",`${message.author} | צריך עזרה`)
-    .addField("__**Voice:**__",`${message.member.voice.channel || "\⛔️ לא בחדר דיבור \⛔️"} `)
-    .addField("__**Roles Staff:**__",`<@&1160557661801168996>`)
-    .addField("__**Reason:**__",`${args.join(" ") || "לא צוינה סיבה | <:0037pi1sppm71:1160594845761536070>"}`)
-    .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-    .setFooter(`${message.author.tag} `,message.author.displayAvatarURL({ dynamic: true }))
-    .setTimestamp()
-   
+      .setColor('#3575ed')
+      .addField("__**User:**__", `${message.author} | צריך עזרה`)
+      .addField("__**Voice:**__", `${message.member.voice.channel || "\⛔️ לא בחדר דיבור \⛔️"} `)
+      .addField("__**Roles Staff:**__", `<@&1160557661801168996>`)
+      .addField("__**Reason:**__", `${args.join(" ") || "לא צוינה סיבה"}`)
+      .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+      .setFooter(`${message.author.tag} `, message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+
     message.channel.send(embed)
   }
 };
